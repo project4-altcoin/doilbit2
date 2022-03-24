29d05d55c9ddc84434b6f67b6cd8dea9be019216
jhin93 <scene1993@gmail.com>
2022-03-24 17:20:04 +0900

    pricelist 컴포넌트 추가

---
+++ client/src/components/PriceList.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+function PriceList() {
+    return(
+        <ul className="list-none p-5">
+            <li className="">60000000</li>
+            <li className="">59000000</li>
+            <li className="">58000000</li>
+            <li className="">57000000</li>
+            <li className="">56000000</li>
+            <li className="">55000000</li>
+            <li className="">54000000</li>
+            <li className="">53000000</li>
+            <li className="">52000000</li>
+            <li className="">51000000</li>
+            <li className="">50000000</li>
+        </ul>
+
+        //rounded-lg shadow p-1
+
+
+        // <div className="bg-white rounded-lg shadow p-10">
+        //     <div className="bg-white rounded-lg shadow p-2">1000000000000000</div>
+        //     <div className="bg-white rounded-lg shadow p-2">2</div>
+        //     <div className="bg-white rounded-lg shadow p-2">3</div>
+        //     <div className="bg-white rounded-lg shadow p-2">4</div>
+        //     <div className="bg-white rounded-lg shadow p-2">5</div>
+        //     <div className="bg-white rounded-lg shadow p-2">6</div>
+        //     <div className="bg-white rounded-lg shadow p-2">7</div>
+        //     <div className="bg-white rounded-lg shadow p-2">8</div>
+        //     <div className="bg-white rounded-lg shadow p-2">9</div>
+        //     <div className="bg-white rounded-lg shadow p-2">10</div>
+        // </div>
+    )
+}
+
+
+export default PriceList;
--- client/src/container/Layout.js
+++ client/src/container/Layout.js
@@ -4,17 +4,21 @@ import BuyOrderForm from "../components/BuyOrderForm";
 import SellOrderform from "../components/SellOrderForm";
 import NewsFeed from "../components/NewsFeed";
 import Bank from "../components/Bank";
+import PriceList from "../components/PriceList";
 
 function Layout() {
     return (
         <div>
             <Nav />
             <div class="relative top-80 justify-center flex flex-wrap">
+                
                 <BuyOrderForm />
+                {/* <PriceList /> */}
                 <SellOrderform />
                 
             </div>
             <Bank />
+            <PriceList />
         </div>
     )
 }
