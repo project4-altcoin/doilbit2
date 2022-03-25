ae0407b8fc560511c687e9ea37df2a261bbe0c9d
jhin93 <scene1993@gmail.com>
2022-03-25 16:22:47 +0900

    깃 되돌리기

--- server/routes/Orders.js
+++ server/routes/Orders.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/orderController");
 const listController = require("../controllers/listController")
+// 되돌리기
 
 router.post('/buy', orderController.buy);
 router.post('/sell', orderController.sell);
