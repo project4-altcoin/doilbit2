ee5de648cd43b4a84753bc79d711b39509ff62c3
jhin93 <scene1993@gmail.com>
2022-03-28 22:12:18 +0900

    혹시 모르니까 백업(axios로매수매도 가격 비교 로직 orderController.js)

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,10 +1,48 @@
 const bank = require('../models/bank');
 const User = require('../models/userModel');
 const OrdersAll = require("../models/ordersAll")
+const axios = require('axios')
 
 exports.trans = (req, res, next) => {
+    // 매수 가격 데이터
+    console.log("what? : ", req.body.buyprice)
+    // 매수 로직
+    // 매수 데이터 가격 == 매도 데이터 check
+
+    var dbselldtarr = [];
+    
+    var dbsellprice = async() => {
+        try {
+            let dbsellraw = await axios.get("http://localhost:3001/exchange/sellapi")
+            // console.log(dbsellraw.data)
+            for(let i = 0; i < dbsellraw.data.length; i ++){
+                // console.log("sellprice: ", dbsellraw.data[i].sellprice)
+                dbselldtarr.push(dbsellraw.data[i].sellprice)
+                console.log("sellprice array : ", dbselldtarr)
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    dbsellprice();
+    console.log("what about here : ", dbselldtarr)
+
+    // function dbsellpriceE () {
+    //     return new Promise(function(resolve){
+    //         let result = dbsellprice()
+    //         resolve(result)
+    //     })
+    // }  
+
+    // dbsellpriceE().then(function(dt){console.log(dt.data)});
+
+    // console.log("dbsellprice : ", dbsellprice)
+    // if(req.body.buyprice == ){
+
+    // }
     OrdersAll.create(req.body)
-        .then(order => {         
+        .then(order => {    
             res.status(201).json({
                 status: 'success',
                 order                
