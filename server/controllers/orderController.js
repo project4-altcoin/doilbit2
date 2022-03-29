de97ae88784c7677cbc4106da637529fceaf7c66
junghyunB <bjh940126@gmail.com>
2022-03-29 10:27:27 +0900

    jh

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -2,14 +2,36 @@ const bank = require('../models/bank');
 const User = require('../models/userModel');
 const OrdersAll = require("../models/ordersAll")
 
-exports.trans = (req, res, next) => {
+exports.trans = async(req, res, next) => {
+    let data = await OrdersAll.find({})
+    let buyquantityarr = 0;
+    let buypricearr = [];
+    // 매수 최고가 구하기
+    for(i = 0; i < data.length; i++) {
+        if(data[i].buyprice !== undefined) buypricearr.push(data[i].buyprice)
+    }
+  
+   var maxbuyprice = Math.max(...buypricearr) //DB 안에 존재하는 매수가격의 최고가  
+
+    // 매수 최고가 수량 구하기
+    for(i = 0; i < data.length; i++){
+        if( data[i].buyprice == maxbuyprice){
+            buyquantityarr += data[i].buyquantity
+        }
+    }
+
+    console.log("buyquantityarr : ", buyquantityarr); //DB 안에 존재하는 매수가격의 최고가 수량
+
+
+    if(req.body.sellprice == maxbuyprice) {
+
+    }
     OrdersAll.create(req.body)
         .then(order => {         
             res.status(201).json({
                 status: 'success',
                 order                
             });
-            console.log(req.body.buyprice)
         })
         .catch(err => {
             res.status(400).json({
